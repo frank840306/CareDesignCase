bbff606bd733d2f656d8c9ce6a31d92f7668311d
yslin <frank840306@gmail.com>
2017-07-11 21:01:17 +0800

    add RF model

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,32 @@
-import pickle
-import json
+import pickle, json, xlrd, csv, os
+
+default_path = {
+	'ROOT_DIR'		: '.',
+	'DATA_DIR'		: './data',
+	'SRC_DIR'		: './src',
+	'MDL_DIR'		: './mdl',
+	'HOSPITALJSON'	: './data/hospitalList.json',
+	'HOSPITAL2MODELJSON'		: './data/hospital2model.json',	# default size = 5 / hospital, from old to new
+}
+
+def XLSX2CSV(xlsx):
+	wb = xlrd.open_workbook(xlsx)
+	sh = wb.sheet_by_name('Sheet1')
+	print(xlsx)
+	print(xlsx.split('.', 1)[0] + '.csv')
+	your_csv_file = open(xlsx.replace('xlsx', 'csv'), 'w', newline='')
+	wr = csv.writer(your_csv_file, quoting=csv.QUOTE_NONE)
+
+	for rownum in range(sh.nrows):
+		# print(sh.row_values(rownum))
+		# print(type(sh.row_values(rownum)))
+		wr.writerow(sh.row_values(rownum))
+		# wr.writerow([bytes(val, 'UTF-8') for val in sh.row_values(rownum)])
+
+	your_csv_file.close()
+
+	# with open(xlsx.split('.', 1)[0] + '.csv', 'w', newline='') as file:
+	# 	writer = csv.writer()		
 
 def writeFile(data, fout):
 	f = open(fout, 'w')
@@ -27,13 +54,9 @@ def readPickle(fin):
 def readJson(fin):
 	with open(fin) as file:
 		text = json.load(file)
-	# content = json.loads(text)
-	# print(content)
 	return text
 
 def writeJson(data, fout):
-	# text = json.dumps(data)
-	# print (text)
 	with open(fout, 'w') as file:
 		json.dump(data, file)
 	return
