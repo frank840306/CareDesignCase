c3f03b68766503a5d1f8a6e085577579f7f090fd
bigyo <b02902131@csie.ntu.edu.tw>
2017-08-04 20:23:15 +0800

    modify deploy

--- run_server.sh
+++ run_server.sh
@@ -15,7 +15,7 @@ if [ $1 = debug ]; then
 elif [ $1 = deploy ]; then
 	host=0.0.0.0
 	port=5000
-	python3 ${src_dir}/server.py -m $1
+	python3 ${src_dir}/server.py -m $1 -n $host -p $port
 elif [ $1 = clean ]; then
 	# remove file
 	echo 'remove file'
