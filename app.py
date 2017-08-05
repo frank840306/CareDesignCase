55f5b45341ea3fbcbe2c8d0fc1c6c137d1e1e6a7
bigyo <b02902131@csie.ntu.edu.tw>
2017-08-05 20:19:28 +0800

    view

--- app.py
+++ app.py
@@ -2,16 +2,21 @@ from flask import Flask
 from datetime import datetime
 app = Flask(__name__)
 
+
+import view.admin as admin
+
 @app.route('/')
 def homepage():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
     return """
-    <h1>Hello heroku</h1>
+    <h1>Hello CareDesign Model</h1>
     <p>It is currently {time}.</p>
-
-    <img src="http://loremflickr.com/600/400" />
     """.format(time=the_time)
+
+@app.route('/admin')
+def route_admin():
+	return admin.hello()
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
