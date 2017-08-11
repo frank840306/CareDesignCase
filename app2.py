5e3c0b2ff6354a8b32891b99d6fda56594e6c101
bigyo <b02902131@csie.ntu.edu.tw>
2017-08-11 20:17:28 +0800

    try not use tensor

--- app2.py
+++ app2.py
@@ -1,12 +1,12 @@
-from src.server import *
+# from src.server import *
 from view.view import *
 from flask import Flask
 
 app = Flask(__name__)
 
-setupLogger()
-logger = logging.getLogger(__name__)
-logger.info('Initial logger')
+# setupLogger()
+# logger = logging.getLogger(__name__)
+# logger.info('Initial logger')
 
 app.config['SECRET_KEY'] = 'devkey'
 app.config['RECAPTCHA_PUBLIC_KEY'] = '6Mfol9cSAAAAADAkodaYl9wvQCwBMr3qGR_PPHcw'
