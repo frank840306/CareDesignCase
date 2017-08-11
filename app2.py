0f6badfa70e6cccfce509397037169aed3262111
bigyo <b02902131@csie.ntu.edu.tw>
2017-08-11 20:14:13 +0800

    remove create app

--- app2.py
+++ app2.py
@@ -11,7 +11,7 @@ logger.info('Initial logger')
 app.config['SECRET_KEY'] = 'devkey'
 app.config['RECAPTCHA_PUBLIC_KEY'] = '6Mfol9cSAAAAADAkodaYl9wvQCwBMr3qGR_PPHcw'
 
-create_app(app)
+# create_app(app)
 create_view_route(app)
 
 
